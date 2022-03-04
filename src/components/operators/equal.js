41d4d12ef06de67e013cea922d88b566828f8e5e
Thomas Cariot <dafrenchie2002@yahoo.fr>
2022-03-04 12:52:00 +0100

    Error resolution

--- src/components/operators/equal.js
+++ src/components/operators/equal.js
@@ -1,8 +1,8 @@
-const Equal = (num1, num2, whichOperator, equal) => {
+const Equal = ({ numberOnHold, num2, whichOperator, equal }) => {
   return (
     <div
       className="right equal box"
-      onClick={() => equal(num1, num2, whichOperator)}
+      onClick={() => equal(numberOnHold, num2, whichOperator)}
     >
       =
     </div>
