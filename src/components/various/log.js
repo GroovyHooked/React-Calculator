6c4a1d6a69b69f48ff4e3f9b97aeaecdb577045a
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-03-06 18:05:59 +0100

    Improving exponent implementation

--- src/components/various/log.js
+++ src/components/various/log.js
@@ -2,8 +2,8 @@ const Log = ({ memory }) => {
   return (
     <div className="logs">
       <div className="title">Memorised operations</div>
-      {memory.map((element) => {
-        return <p>{element}</p>;
+      {memory.map((element, i) => {
+        return <p key={i}>{element}</p>;
       })}
     </div>
   );
